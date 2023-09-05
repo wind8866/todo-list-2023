a1afec592541bfcb45741fc977bf9f60817bdf77
wind8866 <liuzhen1010xyz@gmail.com>
2023-09-05 22:28:46 +0800

    feat: custom elements

---
+++ packages/vue-web-components/src/views/TodoListSearch.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+export interface requestItem {
+  label: string
+  url: string
+}
+export interface Options {}
+</script>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const input = ref()
+</script>
+
+<template>
+  <span>
+    <input type="text" v-model="input" />
+    {{ input }}
+  </span>
+</template>
--- packages/vue-web-components/vite.config.ts
+++ packages/vue-web-components/vite.config.ts
@@ -7,7 +7,14 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          // 将所有带短横线的标签名都视为自定义元素
+          isCustomElement: (tag) => tag.includes('-')
+        }
+      }
+    }),
     vueJsx(),
   ],
   resolve: {
